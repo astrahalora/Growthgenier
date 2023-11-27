77e7b4205682dabb1a3b0a94c6ad6b4e5eaa403b
astrahalora <astrahalora@gmail.com>
2023-11-27 20:15:52 +0200

    Add router

---
+++ client/src/router.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Profile from "./pages/Profile";
+import Project from "./pages/Project";
+import ProjectList from "./pages/ProjectList";
+import ErrorPage from "./pages/ErrorPage";
+import Navigation from "./components/Navigation";
+
+export const router = createBrowserRouter([
+    { 
+        path: "/", 
+        element: <Navigation />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <Profile />
+            },
+            {
+                path: "/project",
+                element: <Project />
+            },
+            {
+                path: "/projects",
+                element: <ProjectList />
+            }
+        ]
+    }
+])
