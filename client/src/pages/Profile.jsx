4de1f38a891e73bc4d25e65e606f8d96b814593d
astrahalora <astrahalora@gmail.com>
2023-11-29 20:57:16 +0200

    Add footer

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,14 @@
 import GrowthFrame from "../components/GrowthFrame";
+import Footer from "../components/Footer";
 
 export default function Profile() {
 
   return (
+    <>
     <div className="container">
       <GrowthFrame/>
     </div>
+    <Footer/>
+    </>
   );
 }
