ec204b3c65177633a900d2b38b147827e9d65940
astrahalora <astrahalora@gmail.com>
2023-11-29 17:43:25 +0200

    Add nav styling

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,15 +1,16 @@
 import { Outlet, NavLink } from "react-router-dom";
+import growthLogo from "../assets/images/growthgen_logo.png"
 
 export default function Navigation() {
     return <>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-md">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <img className="navbar-brand" src={growthLogo} id="logo" alt="Growthgenier Logo"/>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav ms-auto fw-bold">
                         <li className="nav-item">
                             <NavLink to="/" className="nav-link" aria-current="page">Profile</NavLink>
                         </li>
