50d3727bc315ae30212129a01f87bde2e05d13e8
astrahalora <astrahalora@gmail.com>
2023-11-29 20:37:38 +0200

    Style lg for profile

--- client/src/components/GrowthOptions.jsx
+++ client/src/components/GrowthOptions.jsx
@@ -1,14 +1,26 @@
 export default function GrowthOptions() {
-    const completed = 0;
+  const completed = 0;
   return (
-    <div className="col mt-3">
+    <div className="col text-custom-secondary my-auto text-lg-center">
       <div>
-        <div className="d-inline-block p-3">{completed}</div>
-        <p className="d-inline-block">Completed</p>
+        <div>
+          <h5 className="d-inline-block px-2 me-1 bkg-nature">{completed}</h5>
+          <h5 className="d-inline-block">Completed</h5>
+        </div>
+        <div>
+          <h5 className="d-inline-block px-2 me-1 bkg-achievement">
+            {completed}
+          </h5>
+          <h5 className="d-inline-block">In Progress</h5>
+        </div>
       </div>
-      <div>
-        <div className="d-inline-block p-3">{completed}</div>
-        <p className="d-inline-block">In Progress</p>
+      <div className="row mt-lg-4">
+        <div className="col-12">
+          <button className="btn btn-custom">Start Project</button>
+        </div>
+        <div className="col-12 mt-lg-2">
+          <button className="btn btn-custom">Resume Project</button>
+        </div>
       </div>
     </div>
   );
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,7 @@
 export default function ({ title }) {
   return (
     <div className="mx-auto text-center">
-      <h3>Welcome, {title}</h3>
+      <h3 className="text-custom-secondary">Welcome, {title}</h3>
     </div>
   );
 }
