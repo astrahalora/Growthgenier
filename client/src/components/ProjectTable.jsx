2f11a819658308e95188b41ffbb977486c3a0c20
astrahalora <astrahalora@gmail.com>
2023-12-05 19:43:23 +0200

    Refactor: move table to component

--- client/src/components/ProjectTable.jsx
+++ client/src/components/ProjectTable.jsx
@@ -1,3 +1,54 @@
-export default function ProjectTable() {
-    return <h1>Table</h1>
+export default function ProjectTable({ data }) {
+    const handleStatusChange = () => {
+
+    }
+
+    const checkProjectStatus = (projectStatus) => {
+        return projectStatus === false ? "In Progress" : "Completed";
+    }
+
+    const applyProgressStatusBackground = (projectStatus) => {
+        return projectStatus === false ? "bkg-achievement" : "bkg-nature";
+    }
+
+    return <div className="container mt-4">
+        <h2 className="text-center">{data.name}</h2>
+        <table className="table bkg-fill">
+            <thead>
+                <tr>
+                    <th scope="col">Task</th>
+                    <th scope="col">Completed</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.tasks.map((task) => (
+                    <tr key={task._id}>
+                        <td className="lh-lg">{task.taskName}</td>
+                        <td className="lh-lg">
+                            <input
+                                type="checkbox"
+                                defaultChecked={task.taskStatus}
+                                onChange={() => handleStatusChange(task)}
+                            ></input>
+                        </td>
+                        <td>
+                            <button type="button" className="btn btn-custom">
+                                Edit
+                            </button>
+                        </td>
+                        <td>
+                            <button type="button" className="btn btn-custom">
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        <div className={`${applyProgressStatusBackground(data.status)} p-2 text-center`}>
+            <h3>Status: {checkProjectStatus(data.status)}</h3>
+        </div>
+    </div>
 }
--- client/src/pages/CurrentProject.jsx
+++ client/src/pages/CurrentProject.jsx
@@ -1,3 +1,4 @@
+import ProjectTable from "../components/ProjectTable";
 import { useFetch } from "../utilities/useFetch";
 import ErrorPage from "./ErrorPage";
 import Loading from "./Loading";
@@ -5,65 +6,14 @@ import Loading from "./Loading";
 export default function CurrentProject() {
   const { data, isError, isLoading } = useFetch();
 
-  const handleStatusChange = () => {
-
-  }
-
-  const checkProjectStatus = (projectStatus) => {
-    return projectStatus === false ? "In Progress" : "Completed";
-  }
-
-  const applyProgressStatusBackground = (projectStatus) => {
-    return projectStatus === false ? "bkg-achievement" : "bkg-nature";
-  }
-
   return (
     <>
-      { isLoading ? (
+      {isLoading ? (
         <Loading />
       ) : isError ? (
         <ErrorPage />
       ) : (
-          <div className="container mt-4">
-            <h2 className="text-center">{data[data.length-1].name}</h2>
-            <table className="table bkg-fill">
-              <thead>
-                <tr>
-                  <th scope="col">Task</th>
-                  <th scope="col">Completed</th>
-                  <th scope="col">Edit</th>
-                  <th scope="col">Delete</th>
-                </tr>
-              </thead>
-              <tbody>
-                {data[data.length-1].tasks.map((task) => (
-                    <tr key={task._id}>
-                        <td className="lh-lg">{task.taskName}</td>
-                        <td className="lh-lg">
-                            <input 
-                            type="checkbox"
-                            defaultChecked={task.taskStatus}
-                            onChange={() => handleStatusChange(task)}
-                            ></input>
-                        </td>
-                        <td>
-                            <button type="button" className="btn btn-custom">
-                                Edit
-                            </button>
-                        </td>
-                        <td>
-                            <button type="button" className="btn btn-custom">
-                                Delete
-                            </button>
-                        </td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-            <div className={`${applyProgressStatusBackground(data[data.length-1].status)} p-2 text-center`}>
-                <h3>Status: {checkProjectStatus(data[data.length-1].status)}</h3>
-            </div>
-          </div>
+        <ProjectTable data={data[data.length - 1]} />
       )}
     </>
   );
