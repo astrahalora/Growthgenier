607ad2b3402b7afc29cbbdd095a8c1f4dcd88cee
astrahalora <astrahalora@gmail.com>
2023-12-08 13:00:36 +0200

    All filter by project status

--- client/src/pages/ProjectList.jsx
+++ client/src/pages/ProjectList.jsx
@@ -1,13 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Loading from "./Loading";
 import ErrorPage from "./ErrorPage";
 import ProjectPreview from "../components/ProjectPreview";
 
 export default function ProjectList() {
     const [data, setData] = useState();
+    const [filteredData, setFilteredData] = useState();
+    const [dataToDisplay, setDataToDisplay] = useState();
     const [isError, setIsError] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [stateChanged, setStateChaged] = useState(false);
+    const selectElement = useRef();
 
     useEffect(() => {
         const getLastIncompleteProject = async (controller) => {
@@ -18,7 +21,9 @@ export default function ProjectList() {
                 );
                 if (response.status === 200) {
                     const data = await response.json();
-                    setData(data.reverse());
+                    const reversedData = data.reverse();
+                    setData(reversedData);
+                    setDataToDisplay(reversedData);
                 } else {
                     throw new Error("Request failed");
                 }
@@ -35,6 +40,17 @@ export default function ProjectList() {
         return () => abortController.abort();
     }, [stateChanged]);
 
+    const filterByCompletion = () => {
+        const optionName = selectElement.current.value;
+        setDataToDisplay(data);
+        setDataToDisplay(prev =>  [...prev].filter(item => {
+            if(optionName === "Completed") return item.status;
+            if(optionName === "In Progress") return !item.status;
+            return item;
+        }));
+        setFilteredData(dataToDisplay);
+    }
+
     return (
         <>
             {isLoading ? (
@@ -43,7 +59,26 @@ export default function ProjectList() {
                 <ErrorPage />
             ) : (
                 <div className="container d-flex flex-column align-items-center mt-4">
-                    {data?.map(project => (
+                    <div className="row">
+                        <select
+                            name="project-type"
+                            id="project-type"
+                            className="col text-center bkg-achievement p-2"
+                            onChange={filterByCompletion}
+                            ref={selectElement}
+                        >
+                            <option>-- All Projects --</option>
+                            <option>Completed</option>
+                            <option>In Progress</option>
+                        </select>
+                        <input
+                            type="search"
+                            name="search"
+                            id="search"
+                            placeholder="Search..."
+                            className="col ms-2 p-2" />
+                    </div>
+                    {dataToDisplay.length > 0 && dataToDisplay.map(project => (
                         <ProjectPreview project={project} key={project._id} />
                     ))}
                 </div>
