2688094351a19dff6f78bdf674de8f2f40fa0748
astrahalora <astrahalora@gmail.com>
2023-12-10 16:05:30 +0200

    Debug profile page display on mobile

--- client/src/components/GrowthFrame.jsx
+++ client/src/components/GrowthFrame.jsx
@@ -4,23 +4,24 @@ import GrowthOptions from "./GrowthOptions";
 import Welcome from "./Welcome";
 import Loading from "../pages/Loading";
 import ErrorPage from "../pages/ErrorPage";
+import Footer from "./Footer";
 
 export default function GrowthFrame() {
   const { data, isLoading, isError } = useFetch();
 
   return (
-    <div className="container growth-frame">
+    <div className="container p-0 growth-frame">
       { isLoading ? (
         <Loading/>
       ) : isError ? (
         <ErrorPage/>
       ) : (
-      <div className="row mt-4">
+      <div className="row m-0 p-0 mt-4">
         <Welcome projects={data} />
         <GrowthKeeper projects={data} />
         <GrowthOptions projects={data} />
       </div>
-      ) }
+      )}
     </div>
   );
 }
