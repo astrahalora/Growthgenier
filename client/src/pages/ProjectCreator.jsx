b38e5f1346203d402647218d889629fcb275fba9
astrahalora <astrahalora@gmail.com>
2023-12-11 21:07:51 +0200

    Touch up form style

--- client/src/pages/ProjectCreator.jsx
+++ client/src/pages/ProjectCreator.jsx
@@ -59,64 +59,66 @@ export default function ProjectCreator() {
     }
 
     return (
-        <form onSubmit={handleFormSubmit} className="container p-5 form-box mt-4 bkg-fill rounded-3 text-achievement">
-            <div className="row mx-auto">
-                <label htmlFor="name" className="col-2 form-label lh-lg">
-                    Name:
-                </label>
-                <input
-                    className="col-8 p-1 ps-3"
-                    type="text"
-                    name="name"
-                    id="name"
-                    placeholder="Prepare for the Holidays"
-                    ref={nameInputRef}
-                    required
-                />
-            </div>
+        <form onSubmit={handleFormSubmit} className="container p-4 form-box mt-4 bkg-fill rounded-3 text-achievement">
+            <div className="p-5 border-custom">
+                <div className="row mx-auto">
+                    <label htmlFor="name" className="col-2 form-label lh-lg">
+                        Name:
+                    </label>
+                    <input
+                        className="col-8 p-1 ps-3"
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Prepare for the Holidays"
+                        ref={nameInputRef}
+                        required
+                    />
+                </div>
 
-            <div className="row mx-auto">
-                <h4 className="col-12 text-center mt-3">Tasks:</h4>
-                <ul className="col-12 text-center list-unstyled">
-                    {tasks && tasks.length
-                        ? tasks.map((task) => (
-                            <li key={task.id} className="fs-6">
-                                <p className="d-inline">{task.taskName}</p>
-                                <button
-                                    type="button"
-                                    className="btn btn-custom m-1 ms-2"
-                                    onClick={() => deleteTask(task.id)}>
-                                    X
-                                </button>
-                            </li>
-                        )) : null}
-                </ul>
-                <label htmlFor="task" className="col-2 form-label lh-lg">
-                    Task:
-                </label>
-                <input
-                    className="col-8"
-                    type="text"
-                    name="task"
-                    id="task"
-                    placeholder="Shop for presents"
-                    value={newTaskName}
-                    onChange={(e) => setNewTaskName(e.target.value)}
-                />
-                <button
-                    type="button"
-                    className="btn btn-custom col-2"
-                    onClick={addTask}>
-                    Add
-                </button>
-            </div>
+                <div className="row mx-auto">
+                    <h4 className="col-12 text-center mt-3">Tasks:</h4>
+                    <ul className="col-12 text-center list-unstyled">
+                        {tasks && tasks.length
+                            ? tasks.map((task) => (
+                                <li key={task.id} className="fs-6">
+                                    <p className="d-inline">{task.taskName}</p>
+                                    <button
+                                        type="button"
+                                        className="btn btn-custom m-1 ms-2"
+                                        onClick={() => deleteTask(task.id)}>
+                                        X
+                                    </button>
+                                </li>
+                            )) : null}
+                    </ul>
+                    <label htmlFor="task" className="col-2 form-label lh-lg">
+                        Task:
+                    </label>
+                    <input
+                        className="col-8"
+                        type="text"
+                        name="task"
+                        id="task"
+                        placeholder="Shop for presents"
+                        value={newTaskName}
+                        onChange={(e) => setNewTaskName(e.target.value)}
+                    />
+                    <button
+                        type="button"
+                        className="btn btn-custom col-2"
+                        onClick={addTask}>
+                        Add
+                    </button>
+                </div>
 
-            <div className="row d-flex justify-content-center mt-4">
-                <div className="col-4"></div>
-                <button type="submit" className="btn btn-custom col-4">
-                    Start Project
-                </button>
-                <div className="col-4"></div>
+                <div className="row d-flex justify-content-center mt-4">
+                    <div className="col-4"></div>
+                    <button type="submit" className="btn btn-custom col-4">
+                        Start Project
+                    </button>
+                    <div className="col-4"></div>
+                </div>
             </div>
         </form>
     );
