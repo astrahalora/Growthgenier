683ceabe15c948348fa3e9a275c7c2950cae2e3a
astrahalora <astrahalora@gmail.com>
2023-12-13 20:51:48 +0200

    Make project list responsive

--- client/src/components/ProjectPreview.jsx
+++ client/src/components/ProjectPreview.jsx
@@ -6,30 +6,18 @@ export default function ProjectPreview({ project, deleteProject }) {
     const completionWidth = calculateCompletionWidth(project);
     const navigate = useNavigate();
 
-    // const deleteProject = async(projectId) => {
-    //     try {
-    //         const request = await fetch(`http://127.0.0.1:5000/api/growth/${projectId}`, {
-    //           method: 'DELETE'
-    //         });
-    //         const response = await request.json();
-    //         console.log(response);
-    //       } catch (error) {
-    //         console.error('Error deleting project:', error);
-    //       }
-    // }
-
     return (
-        <div className="d-flex">
+        <div className="d-flex flex-column flex-md-row pb-3 pb-md-0">
             <div
                 className={`${checkProjectStatus(project.status, "bkg")}
-                project-preview d-flex mt-3`}>
+                project-preview d-flex flex-column flex-md-row mt-3`}>
                 <img
                     src={checkProjectStatus(project.status, "pic")}
                     alt="Project Status"
                     className="preview-image" />
                 <div className="d-flex flex-column justify-content-center align-items-center w-100 h-100">
                     <h4>{project.name}</h4>
-                    <div className="w-75 d-flex">
+                    <div className="w-75 d-flex mb-3 mb-md-0">
                         <div
                             className="bkg-dark-fill py-2"
                             style={{ width: `${completionWidth}%` }}>
@@ -41,14 +29,14 @@ export default function ProjectPreview({ project, deleteProject }) {
                     </div>
                 </div>
             </div>
-            <div className="bkg-fill mt-3 d-flex flex-column 
-                justify-content-center align-items-center px-4">
-                <button type="button" className="btn btn-custom"
+            <div className="bkg-fill mt-md-3 d-flex flex-md-column 
+                justify-content-center align-items-center px-4 py-3 py-md-0">
+                <button type="button" className="btn btn-custom me-2 me-md-0"
                     onClick={() => navigate(`/project/${project._id}`)}
                 >
                     Resume
                 </button>
-                <button type="button" className="btn btn-custom mt-2"
+                <button type="button" className="btn btn-custom mt-md-2"
                     onClick={() => deleteProject(project._id)}
                 >
                     Delete
