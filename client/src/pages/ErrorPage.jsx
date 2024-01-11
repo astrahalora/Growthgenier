d4382c55eb436f0573fde35df5b4d7ca48179464
astrahalora <astrahalora@gmail.com>
2024-01-11 19:47:48 +0200

    Change mt for Loading and Error

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,6 +1,6 @@
 export default function ErrorPage() {
   return (
-    <div className="h-75 d-flex justify-content-center align-items-center">
+    <div className="mt-5 d-flex justify-content-center align-items-center">
       <h1>Something didn't work as intended!</h1>
     </div>
   );
--- client/src/pages/Loading.jsx
+++ client/src/pages/Loading.jsx
@@ -2,7 +2,7 @@ import loading from "../assets/images/loading.gif";
 
 export default function Loading() {
     return (
-          <div className="h-100 d-flex justify-content-center align-items-center">
+          <div className="mt-5 d-flex justify-content-center align-items-center">
             <img src={loading} alt="Loading" />
           </div>
     );
